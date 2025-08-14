63aef2c49ca43fafa537ab8adfa5fd6cc5f5c961
Master CVV <ddv.manju@gmail.com>
2025-08-14 20:54:01 +0530

    Mongo Databse config

---
+++ MongoDB.sh
@@ -0,0 +1,18 @@
+#Setup the MongoDB repo file
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+# Install MongoDB
+
+dnf install mongodb-org -y
+
+sed -i 's/127.0.0.1/0.0.0.0'/etc/mongod.conf
+
+#Start & Enable MongoDB Service
+
+systemctl enable mongod
+systemctl start mongod
+
+#Restart the service to make the changes effected.
+
+systemctl restart mongod

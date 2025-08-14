fc804844be2556009065c9d76430a7509c7a87da
Master CVV <ddv.manju@gmail.com>
2025-08-14 22:39:49 +0530

    Catalgoue - List of products

---
+++ Cart.sh
@@ -0,0 +1,28 @@
+#Install NodeJS
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+
+cp cart.service /etc/systemd/system/cart.service
+
+#Add application User
+useradd roboshop
+
+#Lets setup an app directory.
+mkdir /app
+rm -f /tmp/cart.zip
+
+#Download the application code to created app directory.
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip
+cd /app
+unzip /tmp/cart.zip
+
+#Lets download the dependencies.
+cd /app
+npm install
+
+#Load the service.
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
---
+++ User.sh
@@ -0,0 +1,29 @@
+#Install NodeJS
+dnf module disable nodejs -y
+dnf module enable nodejs:20 -y
+dnf install nodejs -y
+
+cp user.service /etc/systemd/system/user.service
+
+#Add application User
+useradd roboshop
+
+#Lets setup an app directory.
+mkdir /app
+rm -f /tmp/user.zip
+
+#Download the application code to created app directory.
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
+cd /app
+unzip /tmp/user.zip
+
+#Lets download the dependencies.
+
+cd /app
+npm install
+
+#Load the service.
+systemctl daemon-reload
+systemctl enable user
+systemctl start user

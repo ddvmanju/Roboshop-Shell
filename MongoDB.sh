5da5a0bfebd49f39d18ef7e243a518fcf3496acf
Master CVV <ddv.manju@gmail.com>
2025-08-15 11:43:45 +0530

    Roboshop full project config

--- MongoDB.sh
+++ MongoDB.sh
@@ -6,7 +6,8 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 dnf install mongodb-org -y
 
-sed -i 's/127.0.0.1/0.0.0.0'/etc/mongod.conf
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 
 #Start & Enable MongoDB Service
 

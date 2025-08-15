45be67e8f11f02b9a8366e90a7183f7327c81c8d
Master CVV <ddv.manju@gmail.com>
2025-08-15 16:16:11 +0530

    Learn shell variables

--- Catalogue.sh
+++ Catalogue.sh
@@ -1,9 +1,11 @@
+app_name=catalogue
+
 # Install NodeJS
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
 dnf install nodejs -y
 
-cp catalogue.service /etc/systemd/system/catalogue.service
+cp $app_name.service /etc/systemd/system/$app_name.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 #Add application User
 useradd roboshop
@@ -13,11 +15,11 @@ useradd roboshop
 rm -rf /app
 mkdir /app
 
-rm -f /tmp/catalogue.zip
+rm -f /tmp/$app_name.zip
 #Download the application code to created app directory.
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+curl -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
 cd /app
-unzip /tmp/catalogue.zip
+unzip /tmp/$app_name.zip
 
 #Lets download the dependencies.
 cd /app
@@ -27,8 +29,8 @@ npm install
 systemctl daemon-reload
 
 #Start the service.
-systemctl enable catalogue
-systemctl start catalogue
+systemctl enable $app_name
+systemctl start $app_name
 
 dnf install mongodb-mongosh -y
 

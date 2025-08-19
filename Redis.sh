835fbd7a34b9f65bbc01d4cbe9946c51712b3108
Master CVV <ddv.manju@gmail.com>
2025-08-19 18:20:32 +0530

    echo$? it indicates whether the command ran success or not

--- Redis.sh
+++ Redis.sh
@@ -2,8 +2,10 @@
 dnf module disable redis -y
 dnf module enable redis:7 -y
 dnf install redis -y
+#sed -i -e 's/127.0.0.1/0.0.0.0' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 
-sed -i -e 's/127.0.0.1/0.0.0.0' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+
 
 #Start & Enable Redis Service
 systemctl enable redis
